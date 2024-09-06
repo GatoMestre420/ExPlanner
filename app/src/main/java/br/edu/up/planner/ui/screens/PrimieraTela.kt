fa6acc942fb284e40364cc713aae43749e252df7
GatoMestre420 <luancurt@outlook.com>
2024-09-06 11:02:26 -0300

    planner

---
+++ app/src/main/java/br/edu/up/planner/ui/screens/PrimieraTela.kt
@@ -0,0 +1,12 @@
+package br.edu.up.planner.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun PrimeiraTela(){
+    Text(text = "Primeira Tela \n" +
+            "Esse curso é daora demais", fontSize = 50.sp)
+
+}
---
+++ app/src/main/java/br/edu/up/planner/ui/screens/SegundaTela.kt
@@ -0,0 +1,13 @@
+package br.edu.up.planner.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SegundaTela(){
+    Text(text = "Segunda Tela", fontSize = 50.sp)
+
+
+    }
